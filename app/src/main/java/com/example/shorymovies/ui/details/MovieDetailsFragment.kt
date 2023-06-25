dd2fb856ef84b1aa5489dcaa9290c1870b786bcd
Khawar habib <khawar.habib@smartdubai.com>
2023-06-25 12:43:30 +0400

    Preparing Fragment Home, Movies and Details

--- app/src/main/java/com/example/shorymovies/ui/details/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/shorymovies/ui/details/MovieDetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.shorymovies.ui.home
+package com.example.shorymovies.ui.details
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,34 +7,34 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.shorymovies.databinding.FragmentHomeBinding
+import com.example.shorymovies.databinding.FragmentMovieDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
-
 
 
 /**
  * created by Khawar Habib on 25/06/2023
  *
  *
- * HomeFragment's responsibilities
+ * MovieDetailsFragment's responsibilities
  *
  * 1- Setup self for DI using @AndroidEntryPoint
- * 2- Setup HomeViewModel to fetch super hero characters
- * 3- Display List of characters
+ * 2- Setup HomeViewModel to fetch list of movies for selected character ie. Spider-man
+ * 3- Display Movies in list
  *
  **/
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class MovieDetailsFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentMovieDetailsBinding
 
-    private val viewModel by viewModels<HomeViewModel>()
+    private val viewModel by viewModels<DetailViewModel>()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentHomeBinding.inflate(layoutInflater).also {
+        return FragmentMovieDetailsBinding.inflate(layoutInflater).also {
             binding = it
         }.root
     }
--- app/src/main/java/com/example/shorymovies/ui/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/shorymovies/ui/movies/MoviesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.shorymovies.ui.home
+package com.example.shorymovies.ui.movies
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,35 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.shorymovies.databinding.FragmentHomeBinding
+import com.example.shorymovies.databinding.FragmentMoviesBinding
 import dagger.hilt.android.AndroidEntryPoint
-
 
 
 /**
  * created by Khawar Habib on 25/06/2023
  *
  *
- * HomeFragment's responsibilities
+ * MoviesFragment's responsibilities
  *
  * 1- Setup self for DI using @AndroidEntryPoint
- * 2- Setup HomeViewModel to fetch super hero characters
- * 3- Display List of characters
+ * 2- Setup HomeViewModel to fetch list of movies for selected character ie. Spider-man
+ * 3- Display Movies in list
  *
  **/
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class MoviesFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
 
-    private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: FragmentMoviesBinding
+
+    private val viewModel by viewModels<MoviesViewModel>()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentHomeBinding.inflate(layoutInflater).also {
+        return FragmentMoviesBinding.inflate(layoutInflater).also {
             binding = it
         }.root
     }
