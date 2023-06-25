b32c8bd589b29588a20b13e1395e893060077db1
Khawar habib <khawar.habib@smartdubai.com>
2023-06-25 15:14:31 +0400

    DialogManager renamed and added one more functionality

---
+++ app/src/main/java/com/example/shorymovies/common/DialogManager.kt
@@ -0,0 +1,116 @@
+package com.example.shorymovies.common
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import com.example.shorymovies.R
+
+
+/**
+ * created by Khawar Habib on 25/06/2023
+ *
+ * Common Alert popup Util class to help us show alert conveniently from anywhere
+ *
+ **/
+object DialogManager {
+    fun showMessageDialog(
+        context: Context,
+        msg: String,
+        posClickListener: DialogInterface.OnClickListener? = null
+    ) {
+        val alertDialog = AlertDialog.Builder(context)
+
+        val builder = alertDialog.apply {
+            setMessage(msg)
+            setPositiveButton(context.getString(R.string.ok), posClickListener)
+
+        }.create()
+
+        builder.show()
+
+        builder.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(Color.WHITE)
+        builder.getButton(AlertDialog.BUTTON_POSITIVE)?.setTextColor(Color.WHITE)
+    }
+
+
+    /**
+     * Show Dialog with Pos/Neg buttons
+     */
+    fun showAlertDialog(
+        context: Context,
+        title: String?,
+        message: String?,
+        posClickListener: DialogInterface.OnClickListener? = null,
+        negClickListener: DialogInterface.OnClickListener? = null
+    ) {
+        val alertDialog = AlertDialog.Builder(context).create()
+        if (title != null)
+            alertDialog.setTitle(title)
+        alertDialog.setMessage(message)
+        alertDialog.setButton(
+            AlertDialog.BUTTON_POSITIVE,
+            context.getString(R.string.ok),
+            posClickListener
+        )
+        if (negClickListener != null) {
+            val cancelText = context.getString(R.string.cancel)
+            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, cancelText, negClickListener)
+        }
+        alertDialog.show()
+
+        alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(Color.WHITE)
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.setTextColor(Color.WHITE)
+    }
+
+
+    fun showAlertWithEditField(
+        context: Context,
+        msg: String,
+        pos: String,
+        neg: String,
+        onClick: (String) -> Unit
+    ) {
+
+        /** creating custom editable field for movie search */
+        val layoutInflater = LayoutInflater.from(context)
+        val editLayout: View = layoutInflater.inflate(R.layout.layout_edittext, null)
+
+        val alertDialog = AlertDialog.Builder(context)
+        alertDialog.setMessage(msg)
+
+        alertDialog.setView(editLayout)
+        val edittext = editLayout.findViewById(R.id.editText) as EditText
+
+        val builder = alertDialog.apply {
+            setMessage(msg)
+            setPositiveButton(pos) { dialog, _ ->
+                edittext.text.toString().let {
+
+                    // don't close dialog if empty text
+                    if (it.isNotEmpty()) {
+                        onClick(edittext.text.toString())
+                        dialog.dismiss()
+                    }
+                }
+
+            }
+
+            setNegativeButton(neg) { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        }.create()
+
+        builder.show()
+
+        builder.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(Color.WHITE)
+        builder.getButton(AlertDialog.BUTTON_POSITIVE)?.setTextColor(Color.WHITE)
+
+    }
+}
+
+
