016b0fff62a7cd4132b3d48dfc906fca8d89c1cd
Khawar habib <khawar.habib@smartdubai.com>
2023-06-25 13:12:55 +0400

    MainActivity updated to support Hilt and navigation screen title

--- app/src/main/java/com/example/shorymovies/MainActivity.kt
+++ app/src/main/java/com/example/shorymovies/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.shorymovies
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.shorymovies.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * MainActivity,
+ *
+ * This is the anchor point of any application (could be renamed)
+ *
+ * Handles flow of different screens (fragment)
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -19,17 +28,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        // use custom toolbar
+        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        /** prepare jetpack navigation component to hand fragment's flow (Home,Movies and Detail screens) */
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.findNavController().run {
+            binding.toolbar.setupWithNavController(this, AppBarConfiguration(graph))
+        }
+
+        /** handles screen title, show the title of whichever screen is showing currently */
+        NavController.OnDestinationChangedListener { _, destination, _ ->
+
+
+            binding.toolbar.title = when (destination.id) {
+                R.id.charactersFragment -> getString(R.string.app_name)
+                else -> getString(R.string.movie_listing)
+            }
+        }
     }
 }
