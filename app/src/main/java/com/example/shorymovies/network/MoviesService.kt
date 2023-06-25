eb3f9e01180edd05f895b0074366fa87aba37458
Khawar habib <khawar.habib@smartdubai.com>
2023-06-25 11:24:35 +0400

    setup MoviesService to fetch data

--- app/src/main/java/com/example/shorymovies/network/MoviesService.kt
+++ app/src/main/java/com/example/shorymovies/network/MoviesService.kt
@@ -1,5 +1,14 @@
 package com.example.shorymovies.network
 
+import com.example.shorymovies.common.Constants
+import com.example.shorymovies.network.model.details.MovieDetails
+import com.example.shorymovies.network.model.home.SuperHeroesResponse
+import com.example.shorymovies.network.model.movies.MoviesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
 
 /**
  * created by Khawar Habib on 25/06/2023
@@ -9,7 +18,35 @@ package com.example.shorymovies.network
  * - Request body
  * - Response types
  *
+ * 1- Fetches movies from omdb server
+ * 2- Fetches movie details from omdb server
+ * 3- Fetches Super hero characters from marvel server
+ *
  **/
 interface MoviesService {
+    /**
+     * Fetch movies for keyword 's' used in title/names in omdb server
+     */
+    @GET(".")
+    suspend fun fetchMovies(
+        @Query("s") keyword: String,
+        @Query("apikey") apikey: String = Constants.API_KEY,
+    ): Response<MoviesResponse>
 
+
+    @GET(".")
+    suspend fun fetchMovieDetails(
+        @Query("i") imdbId: String,
+        @Query("apikey") apikey: String = Constants.API_KEY,
+    ): Response<MovieDetails>
+
+
+    @GET
+    suspend fun fetchSuperHeroes(
+        @Url url: String,
+        @Query("apikey") apiKey: String,
+        @Query("hash") hash: String,
+        @Query("ts") ts: String,
+        @Query("limit") limit: Int,
+    ): Response<SuperHeroesResponse>
 }
