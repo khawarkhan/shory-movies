3938ce5a03d9df63ad6d01b206ce5f65097517c8
Khawar habib <khawar.habib@smartdubai.com>
2023-06-25 15:13:33 +0400

    HomeFragment has now option to serach movie if user doesn't like from existsing list

--- app/src/main/java/com/example/shorymovies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/shorymovies/ui/home/HomeFragment.kt
@@ -2,14 +2,22 @@ package com.example.shorymovies.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.shorymovies.common.AppDialog
+import com.example.shorymovies.R
+import com.example.shorymovies.common.DialogManager
 import com.example.shorymovies.common.RecyclerItemClickListener
 import com.example.shorymovies.databinding.FragmentHomeBinding
 import com.example.shorymovies.network.model.Resource
@@ -56,6 +64,8 @@ class HomeFragment : Fragment() {
             observeSuperHeroes()
             fetchSuperHeroes()
         }
+
+        setupMenu()
     }
 
 
@@ -76,20 +86,7 @@ class HomeFragment : Fragment() {
 
             /** Go to Movies screen, to show movies for selected hero */
             viewModel.getCharacterName(index)?.let { name ->
-
-                /** pick super hero name and push Movie listing screen */
-                val homeToPageAction =
-                    HomeFragmentDirections.actionCharacterToMovies(
-                        /**
-                         *  for testing because sometime marvel character don't have movie in omdb server
-                         *
-                         *  and even if you want see differnt movies of your own choice for testing, just change the name
-                         *
-                         * */
-//                    "spider-man"
-                        name
-                    )
-                findNavController().navigate(homeToPageAction)
+                openMovieListing(name)
             }
         })
     }
@@ -116,7 +113,7 @@ class HomeFragment : Fragment() {
                     }
                 }
 
-                is Resource.Error -> AppDialog.showMessageDialog(
+                is Resource.Error -> DialogManager.showMessageDialog(
                     requireContext(),
                     response.message!!
                 )
@@ -127,5 +124,55 @@ class HomeFragment : Fragment() {
                 }
             }
         }
+    }
+
+
+    /**
+     * Open movie listing
+     */
+    private fun openMovieListing(name: String) {
+        /** pick super hero name and push Movie listing screen */
+        val homeToPageAction =
+            HomeFragmentDirections.actionCharacterToMovies(
+                /**
+                 *  for testing because sometime marvel character don't have movie in omdb server
+                 *
+                 *  and even if you want see different movies of your own choice for testing, just change the name
+                 *
+                 * */
+                name
+            )
+        findNavController().navigate(homeToPageAction)
+    }
+
+
+    /**
+     * setting up menu to allow user enter movie if didn't like any in shown listing
+     */
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) {
+            }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                /**
+                 *  open Editable field to let user enter movie name if he/she didn't like one shown in listing
+                 *
+                 * */
+                DialogManager.showAlertWithEditField(
+                    requireActivity(),
+                    getString(R.string.enter_movie_name),
+                    getString(R.string.find),
+                    getString(R.string.cancel),
+                ) { movieName ->
+                    openMovieListing(movieName)
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
