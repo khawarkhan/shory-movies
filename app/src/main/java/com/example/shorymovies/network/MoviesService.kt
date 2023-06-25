639d92e90527707f725d3a1a61642dca74b2a6a6
Khawar habib <khawar.habib@smartdubai.com>
2023-06-25 11:03:09 +0400

    working on project modules structure

---
+++ app/src/main/java/com/example/shorymovies/network/MoviesService.kt
@@ -0,0 +1,13 @@
+package com.example.shorymovies.network
+
+/**
+ * created by Khawar Habib on 25/06/2023
+ *
+ * This is the final service, makes service calls
+ *
+ * - Request body
+ * - Response types
+ *
+ **/
+interface MoviesService {
+}
