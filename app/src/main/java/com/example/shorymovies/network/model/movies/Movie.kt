42abd0da99fe76dcbac36991ee43a3dac10c67aa
Khawar habib <khawar.habib@smartdubai.com>
2023-07-10 07:41:04 +0400

    updated movie object to support room db

--- app/src/main/java/com/example/shorymovies/network/model/movies/Movie.kt
+++ app/src/main/java/com/example/shorymovies/network/model/movies/Movie.kt
@@ -1,8 +1,18 @@
 package com.example.shorymovies.network.model.movies
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "movies_table")
 data class Movie(
+
+    /** imdbID would be the primary key */
+    @PrimaryKey(autoGenerate = false)
+    @SerializedName("imdbID")
+    val imdbID: String,
+
+
     @SerializedName("Poster")
     val Poster: String,
     @SerializedName("Title")
@@ -11,6 +21,10 @@ data class Movie(
     val Type: String,
     @SerializedName("Year")
     val Year: String,
-    @SerializedName("imdbID")
-    val imdbID: String
+
+    /**
+     * key for for db used as foreign key (just to keep movie against it)
+     */
+    @SerializedName("MovieName")
+    var movieName: String?,
 )
