4e19720744d6273a77194d25d5607999fe1719ab
Khawar habib <khawar.habib@smartdubai.com>
2023-07-10 07:40:39 +0400

    added few more functions to AppModule for code cleanup

--- app/src/main/java/com/example/shorymovies/di/AppModule.kt
+++ app/src/main/java/com/example/shorymovies/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.example.shorymovies.di
 
 import android.content.Context
+import android.util.Log
 import com.example.shorymovies.BuildConfig
 import com.example.shorymovies.common.NetworkUtils.getOfflineInterceptor
 import com.example.shorymovies.common.NetworkUtils.getOnlineInterceptor
@@ -10,11 +11,15 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import okhttp3.Cache
 import okhttp3.Interceptor.*
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 
@@ -37,6 +42,12 @@ object AppModule {
     @Singleton
     fun provideMovieApi(client: OkHttpClient): MoviesService {
 
+        client.cache?.urls()?.let { urlIterator ->
+            while (urlIterator.hasNext()) {
+                Log.e("TAG", "cache: ${urlIterator.next()}")
+            }
+        }
+
         return Retrofit.Builder()
             .baseUrl(BuildConfig.OMDB_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -54,8 +65,18 @@ object AppModule {
             .addNetworkInterceptor(getOnlineInterceptor())
             /** for offline caching */
             .addInterceptor(getOfflineInterceptor(context))
+            /** an interceptor for logging requests/response */
+            .addInterceptor(getLogInterceptor())
             .cache(cache)
             .build()
+    }
+
+
+    private fun getLogInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return interceptor
+
     }
 
     @Provides
@@ -65,4 +86,17 @@ object AppModule {
         return Cache(context.cacheDir, cacheSize)
     }
 
+
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob())
+    }
+
 }
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
